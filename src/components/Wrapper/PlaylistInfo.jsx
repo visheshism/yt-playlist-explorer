df8e5b414da0808686f1cf4175a20b91eb375fce
Vishesh Singh <67804974+visheshism@users.noreply.github.com>
2023-08-12 06:01:22 +0530

    fixed unexpected use of spread operator

--- src/components/Wrapper/PlaylistInfo.jsx
+++ src/components/Wrapper/PlaylistInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const PlaylistInfoComp = ({ classes, playlistInfo: { title, description } }) => {
-    return (<div className={...classes.join(' ')} style={{ width: '100%' }}>
+    return (<div className={classes.join(' ')} style={{ width: '100%' }}>
         <h3 className='font-Montserrat' style={{ width: '100%', textAlign: 'center', fontSize: '26px', fontWeight: 550, padding: '10px 0' }}>{title}</h3>
         <p className='text-truncate font-Montserrat' style={{ width: '100%', margin: '10px 0', letterSpacing: '0.8px', marginBottom: '20px', textAlign: 'center', padding: '0 40px' }}>{description}</p>
     </div>
